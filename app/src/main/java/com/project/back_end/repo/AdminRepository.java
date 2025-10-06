d4320c742a13fc2375560653bd3f4dd85d726c60
klougaris <klougaris@gmail.com>
2025-10-06 21:21:32 +0300

    Update AdminRepository.java

--- app/src/main/java/com/project/back_end/repo/AdminRepository.java
+++ app/src/main/java/com/project/back_end/repo/AdminRepository.java
@@ -1,6 +1,6 @@
 package com.project.back_end.repo;
 
-import com.example.yourproject.model.Admin;
+import com.example.yourproject.models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
