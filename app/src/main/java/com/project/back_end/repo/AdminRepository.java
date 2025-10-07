49778f5c47ccd9045c039965e9b04363a532ae71
klougaris <klougaris@gmail.com>
2025-10-07 20:09:36 +0300

    Update AdminRepository.java

--- app/src/main/java/com/project/back_end/repo/AdminRepository.java
+++ app/src/main/java/com/project/back_end/repo/AdminRepository.java
@@ -1,12 +1,12 @@
 package com.project.back_end.repo;
 
-import com.example.yourproject.models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.project.back_end.models.Admin;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
-public Admin findByUsername(String username);
-
+    // Custom query method to find an Admin by username
+    Admin findByUsername(String username);
 }
