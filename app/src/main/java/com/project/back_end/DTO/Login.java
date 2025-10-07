3cb2c2c40d6b980344fa0154604f9acf6c26aee8
klougaris <klougaris@gmail.com>
2025-10-07 20:05:37 +0300

    Update Login.java
    Changed the hole class

--- app/src/main/java/com/project/back_end/DTO/Login.java
+++ app/src/main/java/com/project/back_end/DTO/Login.java
@@ -1,32 +1,34 @@
 package com.project.back_end.DTO;
 
 public class Login {
-    
-    private String email;
 
+    private String identifier;
     private String password;
 
-    // Default constructor (optional, Java provides this automatically)
+    // Default constructor
+    public Login() {}
 
-    public Login(){}
+    // Optional parameterized constructor (useful for tests or service calls)
+    public Login(String identifier, String password) {
+        this.identifier = identifier;
+        this.password = password;
+    }
 
-    // Getter methods 
-    public String getEmail() {
-        return email;
+    // ===== Getters and Setters =====
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
     }
 
     public String getPassword() {
         return password;
     }
 
-    // Setter methods
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
-
 }
